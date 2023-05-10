817b08768448c257de173ca0845b461281265f2b
Umnokisa <kudahtalka@gmail.com>
2023-05-10 14:08:22 +0300

    upd

--- 1.js
+++
@@ -1,3 +0,0 @@
-let celsius = prompt("Введите температуру в градусах Цельсия:");
-let fahrenheit = (celsius * 9/5) + 32;
-alert("Цельсий: " + celsius + ", Фаренгейт: " + fahrenheit);
--- 2.js
+++
@@ -1,8 +0,0 @@
-function Grettings() {
-    let name = prompt('Как вас зовут?');
-    msg = `Привет, ${name}!!`
-    alert(msg)
-};
-
-Grettings();
-document.write(msg);
